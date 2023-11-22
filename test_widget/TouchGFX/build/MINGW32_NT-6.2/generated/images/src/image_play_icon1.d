528f2fbe12f1f819fc39ba9ec76c1ee8a30ed0eb
mokhwasomssi <mokhwasomssi@gmail.com>
2023-11-22 09:11:25 +0900

    Test : create test_widget project

---
+++ test_widget/TouchGFX/build/MINGW32_NT-6.2/generated/images/src/image_play_icon1.d
@@ -0,0 +1,5 @@
+build/MINGW32_NT-6.2/generated/images/src/image_play_icon1.o: \
+ generated/images/src/image_play_icon1.cpp \
+ ../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+
+../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
---
+++ test_widget/TouchGFX/build/STM32F746-DISCO/TouchGFX/generated/images/src/image_stop.d
@@ -0,0 +1,5 @@
+TouchGFX/build/STM32F746-DISCO/TouchGFX/generated/images/src/image_stop.o: \
+ TouchGFX/generated/images/src/image_stop.cpp \
+ Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+
+Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
